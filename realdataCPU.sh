e3e21058c20e8395d630a5adee84c06d551b09c1
boit2009 <zemlenyiboti@gmail.com>
2022-11-08 14:35:47 +0100

    fif bash script

--- realdataCPU.sh
+++ realdataCPU.sh
@@ -3,13 +3,13 @@ for iter in {16,8,4,2}
 do  
     OMP_NUM_THREADS=32 mpirun -np 2 ~reguly/numawrap_omp2 ./mpi.exe 0 ${iter} >real_2_iter_${iter}.txt
     echo "real_2_iter_${iter}txt  done"
-    OMP_NUM_THREADS=16 mpirun --oversubscribe -np 4 --bind-to hwthread ./mpi.exe 0 ${iter} >real_4_iter_${iter}.txt
+    OMP_NUM_THREADS=16 mpirun --oversubscribe -np 4 --bind-to numa ./mpi.exe 0 ${iter} >real_4_iter_${iter}.txt
     echo "real_4_iter_${iter}txt  done"
-    OMP_NUM_THREADS=8 mpirun --oversubscribe -np 8 --bind-to hwthread ./mpi.exe 0 ${iter} >real_8_iter_${iter}.txt
+    OMP_NUM_THREADS=8 mpirun --oversubscribe -np 8 --bind-to numa ./mpi.exe 0 ${iter} >real_8_iter_${iter}.txt
     echo "real_8_iter_${iter}txt  done"
-    OMP_NUM_THREADS=4 mpirun --oversubscribe -np 16 --bind-to hwthread ./mpi.exe 0 ${iter} >real_16_iter_${iter}.txt
+    OMP_NUM_THREADS=4 mpirun --oversubscribe -np 16 --bind-to numa ./mpi.exe 0 ${iter} >real_16_iter_${iter}.txt
     echo "real_16_iter_${iter}txt  done"
-    OMP_NUM_THREADS=2 mpirun --oversubscribe -np 32 --bind-to hwthread ./mpi.exe 0 ${iter} >real_32_iter_${iter}.txt
+    OMP_NUM_THREADS=2 mpirun --oversubscribe -np 32 --bind-to numa ./mpi.exe 0 ${iter} >real_32_iter_${iter}.txt
     echo "real_32_iter_${iter}txt  done"
     OMP_NUM_THREADS=1 mpirun --oversubscribe -np 64 --bind-to hwthread ./mpi.exe 0 ${iter} >real_64_iter_${iter}.txt
     echo "real_64_iter_${iter}txt  done"
